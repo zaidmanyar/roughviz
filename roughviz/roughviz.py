1c606c330bb0b87f2d1e23ba6156b519e8aa569c
zaidmanyar <zaidmanyar@hotmail.com>
2020-10-23 16:08:36 +0530

    git push
     #

--- roughviz/roughviz.py
+++ roughviz/roughviz.py
@@ -65,3 +65,44 @@ def stackedbar(labels, values, plot_svg = False, **kwargs):
     for index, row in pd.concat([labels,values],axis=1).iterrows():
         content.append(row.to_dict())
     generate_template(data, labels.name, content, plot_svg, **kwargs)
+
+# def line(path, y1, y2, y3, plot_svg = False, **kwargs):
+    #data = pkgutil.get_data(__package__, 'template/line.html')
+    #generate_template(path, [])
+
+def line(data, y1, y2, y3, plot_svg = False, **kwargs):
+    template = Template(data.decode("utf-8"))
+    id_name = ''.join(random.choice(string.ascii_lowercase) for i in range(10))
+    output = template.render(id_name = id_name,
+                             y1 = y1,
+                             y2 = y2,
+                             y3 = y3,
+                             kwargs = kwargs)
+    if(plot_svg):
+        svg_id = "svg"+id_name
+        script = """
+        <style>
+        div.output_area img, div.output_area svg{
+        height: 100%!important;
+        }
+        </style>
+        <script>
+            var e = document.getElementById('"""+id_name+"""');
+            var divCheckingInterval = setInterval(function(){
+            if(e.getElementsByTagName('svg').length){
+                clearInterval(divCheckingInterval);
+                e.getElementsByTagName('svg')[0].setAttribute("id", '"""+svg_id+"""');
+                var svgElement = document.getElementById('"""+svg_id+"""');
+                var svgString = new XMLSerializer().serializeToString(svgElement);
+                var decoded = unescape(encodeURIComponent(svgString));
+                var base64 = btoa(decoded);
+                var imgSource = `data:image/svg+xml;base64,${base64}`;
+                e.innerHTML = "<img id='svgplot'>";
+                document.getElementById('svgplot').src = imgSource;       
+            }}, 1);
+        </script>
+        """
+        display(HTML(output))
+        display(HTML(script))
+    else:
+        display(HTML(output))
